2027b689a157690d65e3f412df33ee46694efce7
Osbaldo Aguilar <50221347+osbaldoAguilar@users.noreply.github.com>
2024-10-29 10:22:00 -0400

    Fixed Issue with user and chat

--- app/Listeners/ExampleEventListener.php
+++ app/Listeners/ExampleEventListener.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Events\OurExampleEvent;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ExampleEventListener
 {
